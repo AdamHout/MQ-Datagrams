8f0c72f70cc9ed5f8777bbfd1ddf603a4b1abb91
adam <adam.hout@proton.me>
2023-10-06 19:44:49 -0400

    Pull datagram msg ID's

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
 /*
- * Demo program to recieve datagrams from a local queue
+ * MQDGREC - Demo program to recieve datagrams from a local queue
+ * ---------------------------------------------------------------------------------------------
+ * Description:
+ * This console program receives datagrams consisting of host system resource information from 
+ * a local IBM MQ queue (QM1 / DEV.ADAM)
+ * ----------------------------------------------------------------------------------------------
+ * Date       Author        Description
+ * ----------------------------------------------------------------------------------------------
+ * 10/01/23   A. Hout       Original source
+ * ----------------------------------------------------------------------------------------------
+ * 10/05/23   A. Hout       Add MQMD.MsgId to console output
+ * ----------------------------------------------------------------------------------------------
  */
 
 #include <stdio.h>
@@ -16,6 +27,7 @@ int main(int argc, char **argv)
    char *pUID = "app";                             //MQ client user ID
    char *pPwd = "Vn750a16!";                       //Client password
    char msgBuf[BUF_SIZE];                          //Output message buffer
+   int  ctr;                                       //Index counter
    
    //MQI structures
    MQCNO   cnxOpt = {MQCNO_DEFAULT};               //Connection options  
@@ -32,7 +44,7 @@ int main(int argc, char **argv)
    MQLONG   cmpCde;                                //MQCONNX completion code 
    MQLONG   opnCde;                                //MQOPEN completion code 
    MQLONG   msgLen;                                //Message length received
-   MQLONG   resCde;                                //Reason code  
+   MQLONG   resCde;                                //Reason code 
    
    //-------------------------------------------------------
    //Setup MQ connection options/security
@@ -40,7 +52,7 @@ int main(int argc, char **argv)
    cnxOpt.SecurityParmsPtr = &cnxSec;
    cnxOpt.Version = MQCNO_VERSION_5;
    cnxSec.AuthenticationType = MQCSP_AUTH_USER_ID_AND_PWD;
-   cnxSec.CSPUserIdPtr = pUID;                                  //ID = "app"
+   cnxSec.CSPUserIdPtr = pUID;                                                //ID = "app"
    cnxSec.CSPUserIdLength = 3;
    cnxSec.CSPPasswordPtr = pPwd;
    cnxSec.CSPPasswordLength = 9;
@@ -48,7 +60,7 @@ int main(int argc, char **argv)
    //-------------------------------------------------------
    //Connect to the queue manager; Check for errors/warnings
    //-------------------------------------------------------
-   MQCONNX(pQmg,&cnxOpt,&Hcnx,&cmpCde,&resCde);                 //Queue manager = QM1
+   MQCONNX(pQmg,&cnxOpt,&Hcnx,&cmpCde,&resCde);                              //Queue manager = QM1
 
    if (cmpCde == MQCC_FAILED){
       printf("MQCONNX failed with reason code %d\n",resCde);
@@ -64,7 +76,7 @@ int main(int argc, char **argv)
    //Open the desired message queue for input
    //-------------------------------------------------------
    opnOpt = MQOO_INPUT_AS_Q_DEF | MQOO_FAIL_IF_QUIESCING;
-   strncpy(objDsc.ObjectName,pQue,9);                           //Queue = DEV.ADAM               
+   strncpy(objDsc.ObjectName,pQue,9);                                         //Queue = DEV.ADAM               
    MQOPEN(Hcnx,&objDsc,opnOpt,&Hobj,&opnCde,&resCde);
           
    if (resCde != MQRC_NONE)
@@ -80,35 +92,40 @@ int main(int argc, char **argv)
    }
    
    //-------------------------------------------------------
-   //Pull and process datagrams off of the local queue. 
-   //Terminate after 10 seconds of inactivty
+   //Pull datagrams off of QM1 DEV.ADAM and display them 
+   //on the console. Terminate after 10 seconds of inactivty
    //-------------------------------------------------------
-   getOpt.Version = MQGMO_VERSION_2;                  //Auto clear msg and corr ID                
-   getOpt.MatchOptions = MQMO_NONE;                   //after each MQGET()
+   //getOpt.Version = MQGMO_VERSION_2;                                                          
+   getOpt.Version = MQGMO_CURRENT_VERSION;                                    //Use the most recent version of MQGMO
+   getOpt.MatchOptions = MQMO_NONE;                                           //FIFO retreival                                            
    getOpt.Options = MQGMO_WAIT | MQGMO_NO_SYNCPOINT;   
-   getOpt.WaitInterval = 10000;                       //Wait up to 10 seconds         
+   getOpt.WaitInterval = 10000;                                               //Wait up to 10 seconds         
     
-    do{                                               //Loop until no more messages
-      //msgDsc.Encoding = MQENC_NATIVE;           //Reset to default values
-      //msgDsc.CodedCharSetId = MQCCSI_Q_MGR;
-      MQGET(Hcnx,Hobj,&msgDsc,&getOpt,BUF_SIZE,msgBuf,&msgLen,&cmpCde,&resCde);
+   do{                                                                        //Loop until no more messages
+     msgDsc.Encoding = MQENC_NATIVE;                                         
+     msgDsc.CodedCharSetId = MQCCSI_Q_MGR;
+     MQGET(Hcnx,Hobj,&msgDsc,&getOpt,BUF_SIZE,msgBuf,&msgLen,&cmpCde,&resCde);
    
-      if (resCde != MQRC_NONE){
-         switch(resCde){
-            case MQRC_NO_MSG_AVAILABLE:
-               puts("No messages on the queue");
-               break;
-            case MQRC_TRUNCATED_MSG_FAILED:
-               puts("Message truncated");
-               break;
-            default:
-               printf("MQGET reason code: %d\n",resCde);
-         }
-      }
-      else{  
-         msgBuf[msgLen] = '\0';            /* add terminator          */
-         printf("%s\n",msgBuf); 
-      }
+     if (resCde != MQRC_NONE){
+        switch(resCde){
+           case MQRC_NO_MSG_AVAILABLE:
+              puts("No messages on the queue");
+              break;
+           case MQRC_TRUNCATED_MSG_FAILED:
+              puts("Message truncated");
+              break;
+           default:
+              printf("MQGET reason code: %d\n",resCde);
+        }
+     }
+     else{  
+        printf("Msg ID: ");                                                      //Add MQMD.MsgId to console output
+        for(ctr=0; ctr<MQ_MSG_ID_LENGTH; ctr++)
+           printf("%02X",msgDsc.MsgId[ctr]);
+        printf("\n");
+        msgBuf[msgLen] = '\0';
+        printf("%s\n",msgBuf); 
+     }
    }while(cmpCde != MQCC_FAILED && resCde == MQRC_NONE);
    
    //-------------------------------------------------------
